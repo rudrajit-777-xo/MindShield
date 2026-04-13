d14ac63869561540763961686541aa8a36f87176
rudrajit-777-xo <rudrajitdey134@gmail.com>
2026-04-13 11:57:49 +0530

    Update sentiment.py

---
+++ modules/sentiment.py
@@ -0,0 +1,18 @@
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+# Initialize once
+analyzer = SentimentIntensityAnalyzer()
+
+def get_sentiment(text):
+    scores = analyzer.polarity_scores(text) #scores = {'neg': 0.6,'neu': 0.3,'pos': 0.1,'compound': -0.7}
+
+   
+    compound = scores['compound'] #Take the final compound score from the analysis result
+
+    # Classification
+    if compound >= 0.05:
+        return "Positive", compound
+    elif compound <= -0.05:
+        return "Negative", compound
+    else:
+        return "Neutral", compound
